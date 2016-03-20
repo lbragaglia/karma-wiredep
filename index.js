da167790559b5602b5199ca5f86fac6aa534f761
Rich <rjohnson@goaldesigns.com>
2016-03-19 19:04:10 -0500

    Handle empty option key
    Fixes an error that is thrown when the cwd option is not specified.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function createPattern(path) {
 
 importDependencies.$inject = ['config.files', 'config.wiredep', 'config.basePath'];
 function importDependencies(files, options, basePath) {
-  options.cwd = path.resolve(basePath, options.cwd);  // Relative paths resolve from Karma basePath
+  options.cwd = path.resolve(basePath, options.cwd || '');  // Relative paths resolve from Karma basePath
   wiredep(options).js.slice().reverse().forEach(function(dep) {
     files.unshift(createPattern(dep));
   });
